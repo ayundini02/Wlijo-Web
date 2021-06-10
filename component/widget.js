c938012cf49b4d8971282d71b3d10ed78c700fb7
Khoirul Umam <khoirul.umam35@gmail.com>
2021-06-10 14:36:38 +0700

    Update widget.js
    Saya sederhanakan dengan membuatkan internal komponen CardKategori dan CardSpecialToday sehingga penulisan kode dapat dipersingkat. Penggunakan properti "class" juga harus diupdate menjadi "className"

--- component/widget.js
+++ component/widget.js
@@ -1,208 +1,104 @@
+const CardKategori = props => (
+  <div className="card-box-c foo">
+	  <div className="card-header-c d-flex">
+      <div className="card-box-ico"></div>
+      <div className="card-title-c align-self-center">
+        <h2 className="title-c">{props.kategori}</h2>
+      </div>
+    </div>
+    <div className="card-body-c"></div>
+    <div className="card-footer-c">
+      <a href="#" class="link-c link-icon">Read more<span class="bi bi-chevron-right"></span></a>
+    </div>
+  </div>
+);
+
+const CardSpecialToday = props => (
+  <div className="card-box-a card-shadow">
+    <div className="img-box-a">
+      <img src={props.imgSrc} alt="" className="img-a img-fluid"/>
+    </div>
+    <div className="card-overlay">
+      <div className="card-overlay-a-content">
+        <div className="card-header-a">
+          <h2 className="card-title-a">
+            <a href="#">{props.nama}</a>
+          </h2>
+        </div>
+        <div className="card-body-a">
+          <div className="price-box d-flex">
+            <span className="price-a">Rp. {props.harga}</span>
+          </div>
+        </div>
+        <div className="card-footer-a">
+          <ul className="card-info d-flex justify-content-around">
+            <li><h4 className="card-info-title">Rp. {props.harga}</h4></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 const Widget = () => (
   <main id="main">
-    <section class="section-property section-t8">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="title-wrap d-flex justify-content-between">
-              <div class="title-box">
-                <h2 class="title-a">Kategori</h2>
+    <section className="section-property section-t8">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="title-wrap d-flex justify-content-between">
+              <div className="title-box">
+                <h2 className="title-a">Kategori</h2>
               </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
-                </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Sayuran</h2>
-                </div>
-              </div>
-              <div class="card-body-c">
-              </div>
-              <div class="card-footer-c">
-                <a href="#" class="link-c link-icon">Read more
-                  <span class="bi bi-chevron-right"></span>
-                </a>
-              </div>
-            </div>
+        <div className="row">
+          <div className="col-md-4">
+            <CardKategori kategori="Sayuran" />
           </div>
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
-                </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Buah</h2>
-                </div>
-              </div>
-              <div class="card-body-c">
-              </div>
-              <div class="card-footer-c">
-                <a href="#" class="link-c link-icon">Read more
-                  <span class="bi bi-chevron-right"></span>
-                </a>
-              </div>
-            </div>
+          <div className="col-md-4">
+            <CardKategori kategori="Buah" />
           </div>
-          <div class="col-md-4">
-            <div class="card-box-c foo">
-              <div class="card-header-c d-flex">
-                <div class="card-box-ico">
-                </div>
-                <div class="card-title-c align-self-center">
-                  <h2 class="title-c">Lauk-Pauk</h2>
-                </div>
-              </div>
-              <div class="card-body-c">
-              </div>
-              <div class="card-footer-c">
-                <a href="#" class="link-c link-icon">Read more
-                  <span class="bi bi-chevron-right"></span>
-                </a>
-              </div>
-            </div>
+          <div className="col-md-4">
+            <CardKategori kategori="Lauk-Pauk" />
           </div>
         </div>
-        </div>
-        </section>
+      </div>
+    </section>
 
-        <section class="section-property section-t8">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="title-wrap d-flex justify-content-between">
-              <div class="title-box">
-                <h2 class="title-a">Special To Day</h2>
+    <section className="section-property section-t8">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="title-wrap d-flex justify-content-between">
+              <div className="title-box">
+                <h2 className="title-a">Special To Day</h2>
               </div>
-              <div class="title-link">
+              <div className="title-link">
                 <a href="property-grid.html">All Property
-                  <span class="bi bi-chevron-right"></span>
+                  <span className="bi bi-chevron-right"></span>
                   </a>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="card-box-a card-shadow">
-              <div class="img-box-a">
-                <img src="/img/U1fabdf60ed5f441a876ce00b69452381c.jpg" alt="" class="img-a img-fluid"/>
-              </div>
-              <div class="card-overlay">
-                <div class="card-overlay-a-content">
-                  <div class="card-header-a">
-                    <h2 class="card-title-a">
-                      <a href="#">Wortel</a>
-                    </h2>
-                  </div>
-                  <div class="card-body-a">
-                    <div class="price-box d-flex">
-                      <span class="price-a">Rp. 3000</span>
-                    </div>
-                  </div>
-                  <div class="card-footer-a">
-                    <ul class="card-info d-flex justify-content-around">
-                      <li>
-                        <h4 class="card-info-title">Rp. 3.000</h4>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card-box-a card-shadow">
-              <div class="img-box-a">
-                <img src="/img/tomaotoes1_560x560.jpg" alt="" class="img-a img-fluid"/>
-              </div>
-              <div class="card-overlay">
-                <div class="card-overlay-a-content">
-                  <div class="card-header-a">
-                    <h2 class="card-title-a">
-                      <a href="#">Tomat</a>
-                    </h2>
-                  </div>
-                  <div class="card-body-a">
-                    <div class="price-box d-flex">
-                      <span class="price-a">Rp. 7.000</span>
-                    </div>
-                  </div>
-                  <div class="card-footer-a">
-                    <ul class="card-info d-flex justify-content-around">
-                      <li>
-                        <h4 class="card-info-title">Rp. 7.000</h4>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card-box-a card-shadow">
-              <div class="img-box-a">
-                <img src="/img/pngtree-non-polluted-ingredients-cabbage-png-image_2380139.jpg" alt="" class="img-a img-fluid"/>
-              </div>
-              <div class="card-overlay">
-                <div class="card-overlay-a-content">
-                  <div class="card-header-a">
-                    <h2 class="card-title-a">
-                      <a href="#">Kubis</a>
-                    </h2>
-                  </div>
-                  <div class="card-body-a">
-                    <div class="price-box d-flex">
-                      <span class="price-a">Rp. 7.000</span>
-                    </div>
-                  </div>
-                  <div class="card-footer-a">
-                    <ul class="card-info d-flex justify-content-around">
-                      <li>
-                        <h4 class="card-info-title">Rp. 7.000</h4>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card-box-a card-shadow">
-              <div class="img-box-a">
-                <img src="/img/Jagung-Manis-Kulit.jpg" alt="" class="img-a img-fluid"/>
-              </div>
-              <div class="card-overlay">
-                <div class="card-overlay-a-content">
-                  <div class="card-header-a">
-                    <h2 class="card-title-a">
-                      <a href="#">Jagung</a>
-                    </h2>
-                  </div>
-                  <div class="card-body-a">
-                    <div class="price-box d-flex">
-                      <span class="price-a">Rp. 4.000</span>
-                    </div>
-                  </div>
-                  <div class="card-footer-a">
-                    <ul class="card-info d-flex justify-content-around">
-                      <li>
-                        <h4 class="card-info-title">Rp. 4.000</h4>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="col-md-4">
+          <CardSpecialToday imgSrc="/img/U1fabdf60ed5f441a876ce00b69452381c.jpg" nama="Wortel" harga="3.000" />
         </div>
-              </section>
-        </main>
-
+        <div className="col-md-4">
+          <CardSpecialToday imgSrc="/img/tomaotoes1_560x560.jpg" nama="Tomat" harga="7.000" />
+        </div>
+        <div className="col-md-4">
+          <CardSpecialToday imgSrc="/img/pngtree-non-polluted-ingredients-cabbage-png-image_2380139.jpg" nama="Kubis" harga="7.000" />
+        </div>
+        <div className="col-md-4">
+          <CardSpecialToday imgSrc="/img/Jagung-Manis-Kulit.jpg" nama="Jagung" harga="4.000" />
+        </div>
+      </div>
+    </section>
+  </main>
 );
 
 export default Widget;
